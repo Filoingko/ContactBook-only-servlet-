72d8c1c14c1319bb8901e85f20ced19fd30b1445
Gihan Chathuranga <Gihan Chathuranga@Gihan-PC>
2017-05-05 08:57:16 +0530

    first

---
+++ src/java/contactbook/AddContact.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contactbook;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Gihan Chathuranga
+ */
+public class AddContact extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Add New Contact</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1><center>Servlet Add New Contact</center></h1>");
+            out.println("<form action=\"savetofile\">\n"
+                    + "	<table>\n"
+                    + "	<tr><td>Name :</td><td><input type=\"text\" name=\"name\"></td></tr>\n"
+                    + "	<tr><td>TP Number </td><td><input type=\"number\" name=\"tpnumber\"></td></tr>\n"
+                    + "	<tr><td>E-mail</td><td><input type=\"email\" name=\"email\"></td></tr>\n"
+                    + "	<tr><td></td><td><input type='button' value='Clear'><input type=\"submit\" value=\"ADD\"></td></tr>\n"
+                    + "	</table>\n"
+                    + "</form>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
